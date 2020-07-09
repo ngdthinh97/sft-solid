95c2d96a8f48502b36c9ef7f7ebc95e7faf06369
ngdthinh97 <ngdthinh.97@gmail.com>
2020-07-09 15:27:51 +0700

    Spring.star.io initializr

---
+++ src/main/java/solid/springframework/tng/solidexample/SolidExampleApplication.java
@@ -0,0 +1,13 @@
+package solid.springframework.tng.solidexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidExampleApplication.class, args);
+	}
+
+}
